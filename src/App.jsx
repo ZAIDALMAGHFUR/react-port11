bebd08acf0c3f73d3239a7cd6e30f3d9f89fe218
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2022-10-23 16:58:05 +0700

    normal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
 import React from "react";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Project from "./components/Project";
-import Hireme from "./components/Hireme";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Bot from "./components/Bot";
 
@@ -14,13 +7,6 @@ const App = () => {
     <div>
       <Navbar />
       <Bot />
-      <Hero />
-      <About />
-      <Skills />
-      <Hireme />
-      <Project />
-      <Contact />
-      <Footer />
     </div>
   );
 };
